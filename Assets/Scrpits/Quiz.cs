ee5b1130661f4ca49fce13221cf2bb7a7070b74b
Brian Zuber <zuber573@morrisville.edu>
2022-03-29 12:07:27 -0400

    Button States

--- Assets/Scrpits/Quiz.cs
+++ Assets/Scrpits/Quiz.cs
@@ -14,11 +14,8 @@ public class Quiz : MonoBehaviour
     [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
-        questionText.text = question.GetQuestion();
-        for (int i = 0; i < answerButtons.Length; i++)
-        {TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text = question.GetAnswer(i);
-        }   
+        GetNextQuestion();
+       //DisplayQuestion();
 
     }
     public void OnAnswerSelected(int index)
@@ -43,7 +40,53 @@ public class Quiz : MonoBehaviour
 
 
        } 
+       SetButtonState(false);
 
         
     }
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        { 
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }   
+
+
+    }
+
+    void SetButtonState(bool state)
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+
+        }
+
+
+    }
+
+     async void SetDefaultButtonSprites()
+     {
+         for(int i = 0; i < answerButtons.Length; i++)
+         {
+             Image buttonImage = answerButtons[i].GetComponent<Image>();
+             buttonImage.sprite = defaultAnswerSprite;
+
+         }
+
+     }
+
+
+
 }
