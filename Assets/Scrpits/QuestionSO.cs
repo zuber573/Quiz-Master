7ec65d5ec6dc9bd8c30578b72b1d2bc42afe61d5
Brian Zuber <zuber573@morrisville.edu>
2022-03-29 11:11:01 -0400

    Arrays

--- Assets/Scrpits/QuestionSO.cs
+++ Assets/Scrpits/QuestionSO.cs
@@ -6,8 +6,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+   [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
     public string GetQuestion()
     {
         return question;
     }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+
 }
